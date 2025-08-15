541114b127d99c50a4fb04b0a101aa807096c825
farhasgithub <114756726+farhasgithub@users.noreply.github.com>
2025-08-15 02:19:20 +0000

    HelloFriday

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
-res.send('<h1>Hello again, this is my web application! folks</h1>');
+res.send('<h1>Hello again, this is my New 123 web application! folks</h1>');
 });
 app.listen(port, () => {
 console.log(`Server is running at http://localhost:${port}`);
